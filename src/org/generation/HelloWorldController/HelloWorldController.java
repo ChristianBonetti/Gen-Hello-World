c17d3a127583a5d1558dfa51db66eea5113db364
ChristianBonetti <christian_bonetti@hotmail.com>
2023-05-02 19:01:51 -0300

    hello one

---
+++ src/org/generation/HelloWorldController/HelloWorldController.java
@@ -0,0 +1,26 @@
+package org.generation.HelloWorldController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+
+
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+	@GetMapping
+	public String hello() {
+		return "Salve Mundo";
+	}
+	
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "trabalho em equipe";
+}
+	@GetMapping("/bsm1")
+	public String bsm1() {
+		return "comunicação";
+}}
